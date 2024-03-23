3eab2ea0c7525b55e398a4bfd7bd6d0e240a4056
SEMOG <jgomesmartins@hotmail.com>
2024-03-22 21:18:14 -0300

    first commit

---
+++ src/main/java/com/semog/semog/SemogApplication.java
@@ -0,0 +1,13 @@
+package com.semog.semog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/semog/semog/SemogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.semog.semog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
